c2843d74fbc63925a7e298a694e52bd8ff91cf2d
nimukm <56411178+nimukm@users.noreply.github.com>
2019-10-17 17:31:31 -0400

    Update original.js

--- original.js
+++ original.js
@@ -144,7 +144,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = "responsive_img";
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
