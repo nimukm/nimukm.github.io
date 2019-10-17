2a444743ec4cb631b1843f83c2ae139753282183
nimukm <56411178+nimukm@users.noreply.github.com>
2019-10-17 17:32:42 -0400

    Update original.js

--- original.js
+++ original.js
@@ -144,7 +144,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = "responsive_img";
+  var slides = "responsive_img (images.jpg) (download.jpg) (barbie.jpg)";
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
